6f0f27cc48429d5157b0c3bfb64086b894b6b98e
Robert Crismon <rcrismon12@gmail.com>
2016-04-21 00:43:22 -0700

    Add project files.

---
+++ PA 7/PA_7.c
@@ -0,0 +1,11 @@
+/*******************************************************************************************
+* Programmer: Robert Crismon                                                               *
+* Class: CptS 121, Spring  2016; Lab Section 3                                             *
+* Programming Assignment: PA 7                                                             *
+* Date: March, 2016																		   *
+* Description: This program generates math problems, with 5 levels of difficulty.		   *
+* Collaboration:																		   *
+*******************************************************************************************/
+
+#include "PA_7.h"
+
---
+++ PA 7/PA_7.h
@@ -0,0 +1,18 @@
+/*******************************************************************************************
+* Programmer: Robert Crismon                                                               *
+* Class: CptS 121, Spring  2016; Lab Section 3                                             *
+* Programming Assignment: PA 7                                                             *
+* Date: March, 2016																		   *
+* Description: This program generates math problems, with 5 levels of difficulty.		   *
+* Collaboration:																		   *
+*******************************************************************************************/
+
+#ifndef PA_7_H
+#define PA_7_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+#endif
